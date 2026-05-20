7287ddda4dee21a058be9c6bdaf1c84ed12a0952
owenboreham <bobrossrtx@gmail.com>
2026-05-21 00:21:03 +0100

    refactor: streamline syscall handling by removing platform-specific definitions

--- src/engine/sandbox_policy.hpp
+++ src/engine/sandbox_policy.hpp
@@ -1,50 +1,8 @@
 #pragma once
 #include <cstdint>
 
-#ifdef _WIN32
-    // Windows doesn't have sys/syscall.h, define dummy values for the sandbox matcher
-    #define SYS_read        3
-    #define SYS_write       4
-    #define SYS_open        5
-    #define SYS_close       6
-    #define SYS_gettimeofday 78
-    #define SYS_exit        1
-    #define SYS_fork        2
-    #define SYS_execve      11
-    #define SYS_ioctl       54
-    #define SYS_socket      359
-#else
-    #include <sys/syscall.h>
-#endif
 
-// Fallbacks if not defined by the system
-#ifndef SYS_read
-#define SYS_read        3
-#endif
-#ifndef SYS_write
-#define SYS_write       4
-#endif
-#ifndef SYS_open
-#define SYS_open        5
-#endif
-#ifndef SYS_close
-#define SYS_close       6
-#endif
-#ifndef SYS_gettimeofday
-#define SYS_gettimeofday 78
-#endif
-#ifndef SYS_exit
-#define SYS_exit        1
-#endif
-#ifndef SYS_fork
-#define SYS_fork        2
-#endif
-#ifndef SYS_execve
-#define SYS_execve      11
-#endif
-#ifndef SYS_socket
-#define SYS_socket      359
-#endif
+#include "syscalls.hpp"
 
 namespace demi {
 namespace sandbox {
@@ -64,30 +22,30 @@ public:
     SyscallResult validate_syscall(uint64_t syscall_id) const {
         if (!enable_sandbox_) return SyscallResult::ALLOWED;
 
-        switch (syscall_id) {
+        // Use the guest VM ABI Syscall enum for matching
+        DemiEngine::Syscall sc = static_cast<DemiEngine::Syscall>(static_cast<uint32_t>(syscall_id));
+        switch (sc) {
             // Safe operations
-            case SYS_exit:
-            case SYS_gettimeofday:
-            // Add other safe timing/math syscalls
+            case DemiEngine::Syscall::SYS_EXIT:
+            case DemiEngine::Syscall::SYS_GETTIMEOFDAY:
                 return SyscallResult::ALLOWED;
-                
+
             // I/O operations (Handled by our Jailed VFS instead of host)
-            case SYS_read:
-            case SYS_write:
-            case SYS_open:
-            case SYS_close:
+            case DemiEngine::Syscall::SYS_READ:
+            case DemiEngine::Syscall::SYS_WRITE:
+            case DemiEngine::Syscall::SYS_OPEN:
+            case DemiEngine::Syscall::SYS_CLOSE:
                 return SyscallResult::HANDLED_INTERNALLY;
 
             // Dangerous operations
-            case SYS_fork:
-            case SYS_execve:
-            case SYS_socket:
-            // Add other networking/process syscalls
+            case DemiEngine::Syscall::SYS_FORK:
+            case DemiEngine::Syscall::SYS_EXECVE:
+            case DemiEngine::Syscall::SYS_SOCKET:
                 return SyscallResult::DENIED;
-                
+
             default:
                 // Default deny in sandbox mode
-                return SyscallResult::DENIED; 
+                return SyscallResult::DENIED;
         }
     }
 };
